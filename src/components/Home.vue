c8c915d740a6eefa0d554ecc0de10165585da60c
arthurzin1006 <arthurzinpessoal@gmail.com>
2025-03-11 21:23:43 -0300

    Instalando bootstrap

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,34 @@
-<template>
-<div> Home </div>
+<template>  
+    <div>
+    <div class="container mt-4 text-center">
+    <h2>Bem vindo, {{nomeUsuarioLogado}} </h2>
+    </div>
+        <br>
+        <button @click="deslogar">Sair</button>
+    </div>
 </template>
 
-<script>
-export default {}
+<script>   
+export default {
+    data(){
+        return {
+            nomeUsuarioLogado: ''
+        };
+    },
+    mounted(){
+        const usuarioLogado = localStorage.getItem('dados-usuario-logado');
+        if(usuarioLogado){
+            this.nomeUsuarioLogado = JSON.parse(usuarioLogado).nome;
+        }
+    },
+    methods: {
+        deslogar()
+        {
+            localStorage.removeItem('dados-usuario-logado');
+            this.$router.push('/login');
+        }
+    }
+}
 </script>
 
-<style> </style>
+<style></style>
--- src/router.js
+++ src/router.js
@@ -5,10 +5,32 @@ import Home from '@/components/Home.vue'
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
     mode:"history",
     routes: [
         {path: "/login", component: Login},
-        {path: "/home", component: Home},
+        {
+            path: "/home", 
+            component: Home, 
+            meta: { requireAuth: true}
+        },
     ]
-})
+});
+
+
+//Intercepta a navegação visando validar regras
+router.beforeEach((to, from, next) => {
+    const usuarioLogado = localStorage.getItem('dados-usuario-logado');
+
+
+    //Valida se a rota solicitada estar logado se possui dados de login
+    if(to.matched.some(record => record.meta.requiresAuth ) && !usuarioLogado)
+    {
+        //Se a rota exige autenticação e não há usuário logado, redireciona para login
+        next('/login');
+    }
+    //segue a vida
+    next();
+});
+
+export default router;
